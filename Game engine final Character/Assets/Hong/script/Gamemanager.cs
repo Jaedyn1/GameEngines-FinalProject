e332147f092f8f01113d3abf434959423a55c634
BradHong233 <73842786+BradHong233@users.noreply.github.com>
2020-12-06 06:31:01 -0500

    little change
    create ui button

---
+++ Game engine final Character/Assets/Hong/script/Gamemanager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public Transform characterposition;
+
+
+
+
+
+    public void resetPosition()
+    {
+        characterposition.transform.position = new Vector3(0, 0, 0);
+    }
+}
